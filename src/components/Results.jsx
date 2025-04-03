0809b5fc6ffde954ef8dc122265fd785c1b1b796
Cooper Xu <cxu@hamilton.edu>
2025-04-02 20:14:57 -0400

    deleted scss for results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
-import "./Results.scss";
 import Verification from "./Verification.jsx";
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios"; // Import axios
